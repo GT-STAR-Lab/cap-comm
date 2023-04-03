6d3110e977d0a4038b325a08c21f17e2d0a9edee
Kevin Fu <kfu38@gatech.edu>
2023-04-03 16:17:29 -0400

    unify sacred/tb dir naming convention

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import datetime
 import numpy as np
 import os
 import random
@@ -9,7 +10,7 @@ from sacred.observers import FileStorageObserver, MongoObserver
 from sacred.utils import apply_backspaces_and_linefeeds
 import sys
 import torch as th
-from utils.logging import get_logger
+from utils.logging import get_logger, get_unique_dirname
 import yaml
 from yaml import Loader
 import time
@@ -71,7 +72,7 @@ def config_copy(config):
         return deepcopy(config)
 
 
-if __name__ == '__main__':
+def main():
     params = deepcopy(sys.argv)
     th.set_num_threads(1)
 
@@ -110,29 +111,33 @@ if __name__ == '__main__':
     config_dict = recursive_dict_update(config_dict, env_config)
     config_dict = recursive_dict_update(config_dict, alg_config)
 
-
     try:
         map_name = config_dict["env_args"]["map_name"]
     except:
         map_name = config_dict["env_args"]["key"]    
     
-    
     # now add all the config to sacred
     ex.add_config(config_dict)
 
+    # Save Sacred files to disk
+    unique_token = get_unique_dirname(config_dict['name'], map_name)
 
-
-
-    # Save to disk by default for sacred
-    logger.info("Saving to FileStorageObserver in results/sacred.")
-    file_obs_path = os.path.join(results_path, f"sacred/{config_dict['name']}/{map_name}")
-
-    # ex.observers.append(MongoObserver(db_name="marlbench")) #url='172.31.5.187:27017'))
+    logger.info("Saving to FileStorageObserver in results/sacred/{unique_token}.")
+    file_obs_path = os.path.join(results_path, f"sacred/{unique_token}")
     ex.observers.append(FileStorageObserver.create(file_obs_path))
-    # ex.observers.append(MongoObserver())
-    # print(params)
-    ex.run_commandline(params)
 
+    # saving to disk because MongoObserver requires the Docker image to be set
+    # up, which is hard
+    # https://sacred.readthedocs.io/en/stable/examples.html#docker-setup
+    # however, this is necessary to use Omniboard/other frontends
+    # Tensorboard is configured separately, doesn't provide full Sacred
+    # frontend but useful to see metrics/compare two runs
+    # - Kevin
+    #
+    # ex.observers.append(MongoObserver(db_name="marlbench")) #url='172.31.5.187:27017'))
+    # ex.observers.append(MongoObserver())
+
+    ex.run_commandline(params)
 
     subfolders = [ f.path for f in os.scandir(file_obs_path) if f.is_dir() ]
     # print(subfolders)
@@ -141,3 +146,6 @@ if __name__ == '__main__':
     het_config_path = os.path.join(file_obs_path, last_folder + "/het_agents.yaml")
     with open(het_config_path , 'w') as file:
         documents = yaml.dump(het_config, file)
+
+if __name__ == '__main__':
+    main()
