d6e53d4a31b3b8a2974de69b3635a0c1310918e5
Walker-Howell, David P <dwalkerhowell3@gatech.edu>
2023-05-10 13:33:49 -0400

    Update scripts/mappo/run_mappo_tan_ca.sh
    Co-authored-by: Fu, Kevin <kfu38@gatech.edu>

--- scripts/mappo/run_mappo_tan_ca.sh
+++ scripts/mappo/run_mappo_tan_ca.sh
@@ -2,4 +2,4 @@ user=$USER
 proj_dir="/home/${user}/star_lab/ca-gnn-marl"
 echo $proj_dir
 cd $proj_dir
-python3 src/main.py print_config with 'env_yaml=gymma' 'alg_yaml=mappo_debug' env_args.time_limit=50 env_args.key="mpe:TerrainAwareNavigationCA-v0"
+python3 src/main.py with 'env_yaml=gymma' 'alg_yaml=mappo_debug' env_args.time_limit=50 env_args.key="mpe:TerrainAwareNavigationCA-v0"
