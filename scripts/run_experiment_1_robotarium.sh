f87ee5e96f0f14c3a14535d76b232d7a4d3a3fff
Pierce Howell <pierce.howell@gatech.edu>
2023-05-29 11:44:25 -0400

    experiment 1 script

---
+++ scripts/run_experiment_1_robotarium.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+proj_dir=$(find ~ -name "ca-gnn-marl")
+echo $proj_dir
+cd $proj_dir
+
+echo Please ensure the config settings for agent-id/capability-awareness in \
+./Heterogeneous-MARL-CA/env-name/config.yaml are correct!
+echo Enter to continue
+read temp
+
+# Single-channel GNN
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=9976
+
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=6047
+
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=4126
+
+# Single-channel GAT
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma n_heads=2 agent="gat" \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=9976
+
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma n_heads=2 agent="gat" \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=6047
+
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma n_heads=2 agent="gat" \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=4126
+
+# Dual-channel GNN
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma agent="dual_channel_gnn" \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=9976
+
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma agent="dual_channel_gnn" \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=6047
+
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma agent="dual_channel_gnn" \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=4126
+
+# Dual-channel GAT
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma n_heads=2 agent="dual_channel_gat" \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=9976
+
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma n_heads=2 agent="dual_channel_gat" \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=6047
+
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma n_heads=2 agent="dual_channel_gat" \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=4126
+
+# Capability-awareness no communication
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma num_layers=0 \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=9976
+
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma num_layers=0 \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=6047
+
+python3 src/main.py with alg_yaml=mappo_gnn env_yaml=gymma num_layers=0 \
+env_args.time_limit=1000 env_args.key="robotarium_gym:PredatorCapturePreyGNN-v0" seed=4126
